cc96ae20beb478f0840573d4a39175d6e2556406
narendra <narendra@webdevelopmentpark.com>
2022-03-07 14:05:09 +0530

    updated by Narendra

---
+++ ominipayment/src/main/java/com/wdp/ominipayment/OminiPay.java
@@ -0,0 +1,20 @@
+package com.wdp.ominipayment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+public class OminiPay {
+
+    public static void show(Context context, String username, String password, String key, String details){
+
+        Intent intent = new Intent(context,Payment.class);
+        intent.putExtra("username",username);
+        intent.putExtra("password",password);
+        intent.putExtra("key",key);
+        intent.putExtra("details",details);
+        ((Activity)context).startActivityForResult(intent,500);
+
+    }
+
+}
