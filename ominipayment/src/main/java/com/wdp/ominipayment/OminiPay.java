1ad42eeb5b137eb8510017962ba06da20c03fba8
narendra <narendra@webdevelopmentpark.com>
2022-04-14 14:48:59 +0530

    latest 14 april

--- ominipayment/src/main/java/com/wdp/ominipayment/OminiPay.java
+++ ominipayment/src/main/java/com/wdp/ominipayment/OminiPay.java
@@ -7,14 +7,12 @@ import android.content.Intent;
 public class OminiPay {
 
     public static void PaymentRequest(Context context, String username, String password, String key, String details){
-
         Intent intent = new Intent(context,Payment.class);
         intent.putExtra("username",username);
         intent.putExtra("password",password);
         intent.putExtra("key",key);
         intent.putExtra("details",details);
         ((Activity)context).startActivityForResult(intent,500);
-
     }
 
     public static void Subscription(Context context, String username, String password, String key, String details)
@@ -43,8 +41,6 @@ public class OminiPay {
                                 String key, String customer_name, String customer_email,
                                 String amount, String currency, String remark)
     {
-
-
         Intent intent = new Intent(context,CheckoutActivity.class);
         intent.putExtra("username",username);
         intent.putExtra("password",password);
@@ -55,7 +51,6 @@ public class OminiPay {
         intent.putExtra("currency",currency);
         intent.putExtra("remark",remark);
         ((Activity)context).startActivityForResult(intent,800);
-
     }
 
 }
